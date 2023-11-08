cffc96828881fc0fe59d316914f0a1ab220756c8
ambroise <ambroise.berthe@ikomia.com>
2023-11-08 16:52:14 +0100

    add required packages in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from setuptools import setup
 
 setup(name="onnx_donut",
-      version="0.1.0")
+      version="0.1.0",
+      dependency_links=["https://download.pytorch.org/whl/torch_stable.html"],
+      install_requires=[
+          "transformers >= 4.26.0, <=4.30.0",
+          "torch==1.13.1+cu116; python_version >= '3.10'",
+          "torchvision==0.14.1+cu116; python_version >= '3.10'",
+          "torch==1.9.0+cu111; python_version < '3.10'",
+          "torchvision==0.10.0+cu111; python_version < '3.10'",
+          "timm==0.5.4",
+          "donut-python==1.0.9",
+          "onnx",
+          "onnxruntime"
+      ])
